77eb84564b8ee975766377ab8e284dc1fca692bc
4rthurV <arthur.vignes@epitech.eu>
2025-02-25 16:11:53 +0100

    chore(main.c): added a call to create_2d_map function in main loop

--- src/main.c
+++ src/main.c
@@ -23,53 +23,13 @@ void process_events(game_t *game)
     }
 }
 
-sfVertexArray *create_line(sfVector2f *point1, sfVector2f *point2)
-{
-    sfVertexArray *vertex_array = sfVertexArray_create();
-    sfVertex vertex1 = {.position = *point1, .color = sfWhite};
-    sfVertex vertex2 = {.position = *point2, .color = sfWhite};
-
-    if (!vertex_array)
-        return NULL;
-    sfVertexArray_append(vertex_array, vertex1);
-    sfVertexArray_append(vertex_array, vertex2);
-    sfVertexArray_setPrimitiveType(vertex_array, sfLinesStrip);
-    return vertex_array;
-}
-
-int draw_2d_map(game_t *game, sfVector2f **map,
-    int map_width, int map_height)
-{
-    sfVertexArray *line = NULL;
-
-    for (int y = 0; y < map_height; y++) {
-        for (int x = 0; x < map_width; x++) {
-            if (x < map_width - 1) {
-                line = create_line(&map[y][x], &map[y][x + 1]);
-                if (!line)
-                    return 84;
-                sfRenderWindow_drawVertexArray(game->window, line, NULL);
-                sfVertexArray_destroy(line);
-            }
-            if (y < map_height - 1) {
-                line = create_line(&map[y][x], &map[y + 1][x]);
-                if (!line)
-                    return 84;
-                sfRenderWindow_drawVertexArray(game->window, line, NULL);
-                sfVertexArray_destroy(line);
-            }
-        }
-    }
-    return 0;
-}
-
 void my_world(game_t *game)
 {
     sfColor sfGrey = sfColor_fromRGB(128, 128, 128);
     sfVector2f **tile_map = NULL;
 
     init_game(game);
-    tile_map = create_2d_map(game, MAP_Y, MAP_X);
+    tile_map = create_2d_map(game);
     if (!tile_map) {
         write(2, "Failed to create 2D map.\n", 26);
         return;
@@ -77,10 +37,10 @@ void my_world(game_t *game)
     while (sfRenderWindow_isOpen(game->window)) {
         process_events(game);
         sfRenderWindow_clear(game->window, sfGrey);
-        draw_2d_map(game, tile_map, MAP_X, MAP_Y);
+        draw_2d_map(game, tile_map);
         sfRenderWindow_display(game->window);
     }
-    free_tile_map(tile_map, MAP_Y);
+    free_tile_map(tile_map, MAP_HEIGHT);
 }
 
 int main(int argc, char **argv, char **env)
