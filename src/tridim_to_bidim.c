d37355511dd3531ae8a85c5e69559fcec833d97b
4rthurV <arthur.vignes@epitech.eu>
2025-02-25 16:10:10 +0100

    chore(tridim_to_bidim.c): create a map_array of predetermined size and added some free()s for map_t struct

--- src/tridim_to_bidim.c
+++ src/tridim_to_bidim.c
@@ -7,9 +7,9 @@
 
 #include "my_world.h"
 
-sfVector2f **allocate_iso_map_rows(int map_height)
+sfVector2f **allocate_iso_map_rows(void)
 {
-    sfVector2f **iso_map = malloc(map_height * sizeof(sfVector2f *));
+    sfVector2f **iso_map = malloc(MAP_HEIGHT * sizeof(sfVector2f *));
 
     return iso_map;
 }
@@ -21,11 +21,10 @@ static void free_iso_map_partial(sfVector2f **iso_map, int num_rows)
     free(iso_map);
 }
 
-bool allocate_iso_map_columns(sfVector2f **iso_map,
-    int map_height, int map_width)
+bool allocate_iso_map_columns(sfVector2f **iso_map)
 {
-    for (int y = 0; y < map_height; y++) {
-        iso_map[y] = malloc(map_width * sizeof(sfVector2f));
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        iso_map[y] = malloc(MAP_WIDTH * sizeof(sfVector2f));
         if (!iso_map[y]) {
             free_iso_map_partial(iso_map, y);
             return false;
@@ -34,20 +33,46 @@ bool allocate_iso_map_columns(sfVector2f **iso_map,
     return true;
 }
 
-void fill_iso_map(game_t *game, sfVector2f **iso_map,
-    int map_height, int map_width)
+static int free_map_array_error(int **map, int y)
+{
+    if (!map[y]) {
+        for (int i = 0; i < y; i++)
+            free(map[i]);
+        free(map);
+        return 1;
+    }
+    return 0;
+}
+
+int **create_zeroed_map(void)
+{
+    int **map = malloc(MAP_HEIGHT * sizeof(int *));
+
+    if (!map)
+        return NULL;
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        map[y] = malloc(MAP_WIDTH * sizeof(int));
+        if (free_map_array_error(map, y))
+            return NULL;
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            map[y][x] = 0;
+        }
+    }
+    return map;
+}
+
+void fill_iso_map(game_t *game, sfVector2f **iso_map)
 {
     int y = 0;
     int x = 0;
     double base_x = game->window_size.x / 2;
     double base_y = game->window_size.y / 2;
-    static int map[MAP_Y][MAP_X] = {
-        {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 70, 80, 200, 120, 80},
-        {0, 0, 0, 120, 30, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}
-    };
+    int **map = create_zeroed_map();
 
-    for (y = 0; y < map_height; y++) {
-        for (x = 0; x < map_width; x++) {
+    if (!map)
+        return;
+    for (y = 0; y < MAP_HEIGHT; y++) {
+        for (x = 0; x < MAP_WIDTH; x++) {
             iso_map[y][x].x = base_x +
                 project_iso_point_x(game, x * TILE_SIZE, y * TILE_SIZE);
             iso_map[y][x].y = base_y +
@@ -55,17 +80,20 @@ void fill_iso_map(game_t *game, sfVector2f **iso_map,
                 y * TILE_SIZE, map[y][x]);
         }
     }
+    for (y = 0; y < MAP_HEIGHT; y++)
+        free(map[y]);
+    free(map);
 }
 
-sfVector2f **create_2d_map(game_t *game, int map_height, int map_width)
+sfVector2f **create_2d_map(game_t *game)
 {
     sfVector2f **iso_map = NULL;
 
-    iso_map = allocate_iso_map_rows(map_height);
+    iso_map = allocate_iso_map_rows();
     if (!iso_map)
         return NULL;
-    if (!allocate_iso_map_columns(iso_map, map_height, map_width))
+    if (!allocate_iso_map_columns(iso_map))
         return NULL;
-    fill_iso_map(game, iso_map, map_height, map_width);
+    fill_iso_map(game, iso_map);
     return iso_map;
 }
