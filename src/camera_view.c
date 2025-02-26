caad1daef6a7f30c50f52be8e9fa5bb22d367bdb
4rthurV <arthur.vignes@epitech.eu>
2025-02-26 11:12:35 +0100

    feat(camera_view.c): added a function that increase or decrease the height of one tile with mouse left/right click

---
+++ src/camera_view.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** G-ING-200-LIL-2-1-myworld-arthur.vignes
+** File description:
+** camera_view.c
+*/
+
+#include "my_world.h"
+
+void update_view_key_arrows(game_t *game, map_t *map)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyLeft)) {
+        game->angle_x += 10;
+        fill_iso_map(game, map);
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyRight)) {
+        game->angle_x -= 10;
+        fill_iso_map(game, map);
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyUp)) {
+        game->angle_y += 10;
+        fill_iso_map(game, map);
+    }
+    if (sfKeyboard_isKeyPressed(sfKeyDown)) {
+        game->angle_y -= 10;
+        fill_iso_map(game, map);
+    }
+}
