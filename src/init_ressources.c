74d34681475db8a73c0429aaeeafda90bf7b3da2
4rthurV <arthur.vignes@epitech.eu>
2025-03-02 15:21:21 +0100

    chore(init_ressources.c): changed create_zeroed_map() parameters

--- src/init_ressources.c
+++ src/init_ressources.c
@@ -30,7 +30,7 @@ map_t *init_map(void)
         return NULL;
     map->map_height = MAP_HEIGHT;
     map->map_width = MAP_WIDTH;
-    map->array_map = create_zeroed_map();
+    map->array_map = create_zeroed_map(map->map_height, map->map_width);
     if (!map->array_map) {
         free(map);
         return NULL;
