d38a2a99f1ce888b1e76e6a039a5d84fa86ccf57
4rthurV <arthur.vignes@epitech.eu>
2025-03-04 12:03:52 +0100

    chore(init_ressources.c): init map texture by default

--- src/init_ressources.c
+++ src/init_ressources.c
@@ -14,6 +14,7 @@ void init_game(game_t *game)
     game->mode = (sfVideoMode){1920, 1080, 32};
     game->window = sfRenderWindow_create(game->mode, "my_world",
         sfResize | sfClose, NULL);
+    sfRenderWindow_setFramerateLimit(game->window, 60);
     game->window_size = (sfVector2f){1920, 1080};
     game->angle_x = 30;
     game->angle_y = 20;
@@ -22,15 +23,34 @@ void init_game(game_t *game)
     free(mode);
 }
 
+int init_map_texture(map_t *map)
+{
+    map->texture =
+        sfTexture_createFromFile("graphics/textures/grass.png", NULL);
+    if (!map->texture) {
+        printf("Error: Failed to load texture\n");
+        return 0;
+    }
+    sfTexture_setRepeated(map->texture, sfTrue);
+    return 1;
+}
+
+void init_map_dimensions(map_t *map)
+{
+    map->map_height = MAP_HEIGHT;
+    map->map_width = MAP_WIDTH;
+    map->tile_size = TILE_SIZE;
+}
+
 map_t *init_map(void)
 {
     map_t *map = malloc(sizeof(map_t));
 
     if (!map)
         return NULL;
-    map->map_height = MAP_HEIGHT;
-    map->map_width = MAP_WIDTH;
-    map->tile_size = TILE_SIZE;
+    init_map_dimensions(map);
+    if (!init_map_texture(map))
+        return NULL;
     map->array_map = create_zeroed_map(map->map_height, map->map_width);
     if (!map->array_map) {
         free(map);
