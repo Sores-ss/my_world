382db548c7bab6e4d7a284527352c390f65f2525
4rthurV <arthur.vignes@epitech.eu>
2025-03-05 16:15:04 +0100

    chore(texture_mode.c): added possibility to switch from texture mode to terraform mode

--- src/texture_mode.c
+++ src/texture_mode.c
@@ -7,7 +7,7 @@
 
 #include "my_world.h"
 
-void reset_texture_states(buttons_t *buttons)
+static void reset_texture_states(buttons_t *buttons)
 {
     buttons_t *temp = buttons;
 
@@ -15,7 +15,8 @@ void reset_texture_states(buttons_t *buttons)
         if (strcmp(temp->name, "state_grass") == 0 ||
             strcmp(temp->name, "state_water") == 0 ||
             strcmp(temp->name, "state_stone") == 0 ||
-            strcmp(temp->name, "state_sand") == 0) {
+            strcmp(temp->name, "state_sand") == 0 ||
+            strcmp(temp->name, "state_textures") == 0) {
             temp->phase = 0;
             switch_textures(temp);
         }
@@ -68,4 +69,54 @@ void handle_texture_buttons(game_t *game, buttons_t *buttons)
         }
         current = current->next;
     }
+}
+
+static void exit_texture_mode(game_t *game, buttons_t *current,
+    buttons_t *buttons, char *new_mode)
+{
+    current->phase = 0;
+    switch_textures(current);
+    change_mode_name(game, new_mode);
+    reset_texture_states(buttons);
+}
+
+void turn_terraform_mode(buttons_t *buttons)
+{
+    buttons_t *current = buttons;
+
+    if (!current)
+        return;
+    while (current) {
+        if (strcmp(current->name, "state_terraform") == 0) {
+            current->phase = 2;
+            switch_textures(current);
+            flip_other_buttons_state(buttons, current->name);
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void handle_texture_mode(game_t *game, buttons_t *buttons)
+{
+    buttons_t *current = buttons;
+
+    if (!current)
+        return;
+    while (current && current->name) {
+        if (strcmp(current->name, "state_textures") == 0
+            && button_is_clicked(game, current)) {
+            exit_texture_mode(game, current, buttons, "view");
+            return;
+        }
+        if (strcmp(current->name, "state_terraform") == 0
+            && button_is_clicked(game, current)) {
+            exit_texture_mode(game, current, buttons, "terraform");
+            turn_terraform_mode(buttons);
+            return;
+            }
+        current = current->next;
+    }
+    if (strcmp(game->state_mode, "textures") == 0)
+        handle_texture_buttons(game, buttons);
 }
