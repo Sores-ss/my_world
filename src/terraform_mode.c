a504e80f77d0cf4277652fdb44e876ec7e878ec2
4rthurV <arthur.vignes@epitech.eu>
2025-03-05 16:14:24 +0100

    chore(terraform_mode.c): added possibility to switch from terraform mode to texture mode

--- src/terraform_mode.c
+++ src/terraform_mode.c
@@ -7,11 +7,26 @@
 
 #include "my_world.h"
 
-static void handle_terraform_exit(game_t *game, buttons_t *current)
+static void reset_terraform_states(buttons_t *buttons)
+{
+    buttons_t *temp = buttons;
+
+    while (temp) {
+        if (strcmp(temp->name, "state_terraform") == 0) {
+            temp->phase = 0;
+            switch_textures(temp);
+        }
+        temp = temp->next;
+    }
+}
+
+static void exit_terraform_mode(game_t *game, buttons_t *current,
+    buttons_t *buttons, char *new_mode)
 {
     current->phase = 0;
     switch_textures(current);
-    change_mode_name(game, "view");
+    change_mode_name(game, new_mode);
+    reset_terraform_states(buttons);
 }
 
 void reset_map_to_flat(game_t *game, map_t *map)
@@ -44,6 +59,39 @@ void handle_reset_map(game_t *game, map_t *map, buttons_t *buttons)
     }
 }
 
+void turn_texture_mode(buttons_t *buttons)
+{
+    buttons_t *current = buttons;
+
+    if (!current)
+        return;
+    while (current) {
+        if (strcmp(current->name, "state_textures") == 0) {
+            current->phase = 2;
+            switch_textures(current);
+            flip_other_buttons_state(buttons, current->name);
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void handle_mode_button_clicks(game_t *game, buttons_t *buttons,
+    buttons_t *current)
+{
+    if (strcmp(current->name, "state_terraform") == 0
+        && button_is_clicked(game, current) && current->phase == 2) {
+        exit_terraform_mode(game, current, buttons, "view");
+        return;
+    }
+    if (strcmp(current->name, "state_textures") == 0
+        && button_is_clicked(game, current)) {
+        exit_terraform_mode(game, current, buttons, "textures");
+        turn_texture_mode(buttons);
+        return;
+    }
+}
+
 void handle_terraform_mode(game_t *game, buttons_t *buttons, map_t *map)
 {
     buttons_t *current = buttons;
@@ -57,11 +105,7 @@ void handle_terraform_mode(game_t *game, buttons_t *buttons, map_t *map)
         view_point_events(map, game);
     handle_reset_map(game, map, buttons);
     while (current && current->name) {
-        if (strcmp(current->name, "state_terraform") == 0
-            && button_is_clicked(game, current) && current->phase == 2) {
-            handle_terraform_exit(game, current);
-            return;
-        }
+        handle_mode_button_clicks(game, buttons, current);
         current = current->next;
     }
 }
