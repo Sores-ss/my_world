b48f1cbd72ea0d5e3f153f1e5829ea92114dd521
Eros <eros.delianne-le-boucher@epitech.eu>
2025-03-05 14:56:56 +0100

    chore(free and init_ressources): changed to handle new functions

--- src/init_ressources.c
+++ src/init_ressources.c
@@ -26,7 +26,7 @@ void init_game(game_t *game)
 int init_map_texture(map_t *map)
 {
     map->texture =
-        sfTexture_createFromFile("graphics/textures/grass.png", NULL);
+    sfTexture_createFromFile("graphics/textures/grass.png", NULL);
     if (!map->texture) {
         printf("Error: Failed to load texture\n");
         return 0;
@@ -42,6 +42,64 @@ void init_map_dimensions(map_t *map)
     map->tile_size = TILE_SIZE;
 }
 
+static bool free_allocated_texture_rows(map_t *map, int y)
+{
+    for (int j = 0; j < y; j++)
+        free(map->tile_textures[j]);
+    free(map->tile_textures);
+    return false;
+}
+
+static bool allocate_texture_row(map_t *map, int y)
+{
+    map->tile_textures[y] = malloc(sizeof(sfTexture *) * map->map_width);
+    if (!map->tile_textures[y])
+        return false;
+    for (int x = 0; x < map->map_width; x++)
+        map->tile_textures[y][x] = NULL;
+    return true;
+}
+
+bool init_tile_textures(map_t *map)
+{
+    if (!map)
+        return false;
+    map->tile_textures = malloc(sizeof(sfTexture **) * map->map_height);
+    if (!map->tile_textures)
+        return false;
+    for (int y = 0; y < map->map_height; y++) {
+        if (!allocate_texture_row(map, y))
+            return free_allocated_texture_rows(map, y);
+    }
+    return true;
+}
+
+static bool init_map_components(map_t *map)
+{
+    map->array_map = create_zeroed_map(map->map_height, map->map_width);
+    if (!map->array_map)
+        return false;
+    map->iso_map = allocate_iso_map_rows();
+    if (!map->iso_map || !allocate_iso_map_columns(map->iso_map)) {
+        free(map->array_map);
+        return false;
+    }
+    return true;
+}
+
+static bool setup_map_resources(map_t *map)
+{
+    if (!init_map_texture(map))
+        return false;
+    if (!init_map_components(map))
+        return false;
+    if (!init_tile_textures(map)) {
+        free_maps(map->array_map, map->iso_map, map->map_height);
+        return false;
+    }
+    return true;
+}
+
 map_t *init_map(void)
 {
     map_t *map = malloc(sizeof(map_t));
@@ -49,16 +107,7 @@ map_t *init_map(void)
     if (!map)
         return NULL;
     init_map_dimensions(map);
-    if (!init_map_texture(map))
-        return NULL;
-    map->array_map = create_zeroed_map(map->map_height, map->map_width);
-    if (!map->array_map) {
-        free(map);
-        return NULL;
-    }
-    map->iso_map = allocate_iso_map_rows();
-    if (!map->iso_map || !allocate_iso_map_columns(map->iso_map)) {
-        free(map->array_map);
+    if (!setup_map_resources(map)) {
         free(map);
         return NULL;
     }
